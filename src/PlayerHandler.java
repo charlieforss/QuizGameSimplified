839e87c8e52fac5f05c58e754879a4a6674789ec
Charlie <charlieforss@gmail.com>
2024-12-02 12:22:11 +0100

    Snabb på kritan. redo

---
+++ src/PlayerHandler.java
@@ -0,0 +1,3 @@
+public class PlayerHandler{
+
+}
--- src/Server.java
+++ src/Server.java
@@ -1,32 +1,3 @@
-import java.net.Socket;
-import java.net.*;
-import java.io.*;
-import java.util.*;
+public class Server{
 
-public class Server {
-    private static final int PORT = 8080;
-
-    public Server() {
-
-    }
-
-    public void start(){
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("New client connected from: " + clientSocket.getInetAddress().getHostAddress());
-                new Thread(new ClientHandler(clientSocket)).start();
-            }
-
-        }
-        catch (IOException e) {
-            System.err.println("Server error: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.start();
-    }
 }

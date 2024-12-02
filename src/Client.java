23f728186a5fde54f8967347cd97dfbd77c316a6
Charlie <charlieforss@gmail.com>
2024-12-02 14:07:20 +0100

    start on gui

--- src/Client.java
+++ src/Client.java
@@ -6,33 +6,70 @@ import java.net.UnknownHostException;
 
 public class Client{
 
-    private final String serverAddress = "localHost";
-    private final int serverPort = 8080;
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private ClientGUI ClientGUI
 
-
-    private void connectToServer(){
+    public Client(String serverAddress, int serverPort){
         try {
-            Socket socket = new Socket(serverAddress, serverPort);
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            out.flush();
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-        } catch (IOException e) {
-            System.out.println("Can not connect");
-            System.exit(1);
+            socket = new Socket(serverAddress, serverPort);
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+            CLientGUI = new ClientGUI(this);
+        }
+        catch (IOException e){
+            e.printStackTrace();
         }
     }
 
-    private void sendAnswer(){
+    public void Start(){
+        questionListener();
+        ClientGUI.setVisible(true);
+    }
 
+    private void questionListener(){
+        new Thread(() -> {
+            try {
+                while (true){
+                    Question question = (Question) in.readObject();
+                    System.out.println("Recieved question from server: " + question);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        ).start();
+    }
+
+    private void sendAnswer(){
+        try {
+            out.writeObject("Answer");
+            out.writeObject("Correct answer");
+            out.flush();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     private void recieveResault(){
+        try {
+            String resault = (String) in.readObject();
+            System.out.println("Recieved result from server: " + resault);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
 
     public static void main(String[] args) {
-        Client client = new Client();
-        new Client();
+        Client client = new Client("localhost", 8080);
+        client.Start();
+
     }
 
 }
